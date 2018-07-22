04ac2beb878864e2bf97fc728d13b8142f7bf6b0
pquinteroc <patricio.quintero@fox.com>
2018-07-22 01:41:04 -0400

    Add hello world exaple

---
+++ vertx-http-server/src/main/java/com/fox/vertx/ExampleHttpServerVerticle.java
@@ -0,0 +1,105 @@
+package com.fox.vertx;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.shareddata.LocalMap;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+
+public class ExampleHttpServerVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleHttpServerVerticle.class);
+
+    /** Nombre de la funcionalidad del contador */
+    private static final String COUNTER_PARAM_NAME = "counter";
+
+    @Override
+    public void start(Future<Void> future) throws Exception {
+        Router router = Router.router(vertx);
+
+        // Obtenemos las propiedades de configuración del fichero o los valores por defecto
+        String host = config().getString("vertx.host", "0.0.0.0");
+        int port = config().getInteger("vertx.port", 7777);
+
+        /* Cualquier petición hacemos que que responda esto */
+        router.get("/test/*").handler(context -> {
+            LOGGER.info("Executing mapping /test/*");
+            context.next();
+        });
+
+        router.get("/test/test1").handler(context -> {
+            LOGGER.info("Executing mapping /test/test1");
+            String payload = new JsonObject().put("hello", "Hello world !!!").encode();
+            context.response().putHeader("content-type", "application/json").end(payload);
+        });
+
+        router.get("/ping").handler(context -> {
+            LOGGER.info("Executing mapping /ping");
+            String payload = new JsonObject().put("uuid", UUID.randomUUID().toString()).encode();
+            context.response().putHeader("content-type", "application/json").end(payload);
+        });
+
+        /* Endpoin de incrementar en uno un valor en el bus */
+        router.get("/add/:counter").handler(this::addOneInSharedDataBus);
+
+        // creamos el HTTP server
+        vertx.createHttpServer().requestHandler(router::accept).listen(port, host, ar -> {
+            if (ar.succeeded()) {
+                future.complete();
+                LOGGER.info(String.format("Our http server is running at %d", port));
+            } else {
+                future.fail(ar.cause());
+                LOGGER.info(String.format("Our http server is not running !!!!!!!! at %d", port));
+
+            }
+        });
+
+    }
+
+
+    /**
+     * Sumaremos uno al valor existente en la información compartida en el Bus
+     *
+     * @param context
+     */
+    private void addOneInSharedDataBus(RoutingContext context) {
+        try {
+            /* Obtenemos el path param */
+
+            @Nullable
+            String param = context.request().getParam(COUNTER_PARAM_NAME);
+            int increase = Integer.valueOf(param);
+
+            /* Local map dentro el bus */
+            LocalMap<String, String> map = vertx.sharedData().getLocalMap("myMap");
+
+            /* Obtenemos el valor actual*/
+            String stringCounter = map.get(COUNTER_PARAM_NAME);
+            Integer counter = increase;
+            if (StringUtils.isNotBlank(stringCounter)){
+                counter += Integer.valueOf(stringCounter);
+            }
+
+            /* Ponemos el valor */
+            map.put(COUNTER_PARAM_NAME, String.valueOf(counter));
+            LOGGER.debug("Valor del counter " + counter);
+
+            /* Payload de respuesta */
+            String payload = new JsonObject().put(COUNTER_PARAM_NAME, counter).encode();
+            context.response().putHeader("content-type", "application/json").end(payload);
+
+        } catch (Exception ex) {
+            context.fail(400);
+        }
+    }
+
+}
---
+++ vertx-http-server/src/main/java/com/fox/vertx/ExampleMainVerticle.java
@@ -0,0 +1,38 @@
+package com.fox.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+
+public class ExampleMainVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleMainVerticle.class);
+
+    @Override
+    public void start(Future<Void> future) throws Exception {
+
+        /* Http server verticle */
+        String verticleName = ExampleHttpServerVerticle.class.getName();
+
+        /* Opciones del servidor http */
+        DeploymentOptions options = new DeploymentOptions();
+        /* Asignamos como instancias el numero de procesadores por 2 (Patrón multireactor)
+         * http://vertx.io/docs/vertx-core/java/#_specifying_number_of_verticle_instances */
+        options.setConfig(config());
+        //options.setInstances(Runtime.getRuntime().availableProcessors() * 2);
+        //options.setInstances(1);
+
+        vertx.deployVerticle(verticleName, options, ar -> {
+            if (ar.succeeded()) {
+                LOGGER.info(String.format("Deployment verticle %s ok ", verticleName));
+                future.complete();
+            } else {
+                LOGGER.info(String.format("Deployment verticle %s ko ", verticleName));
+                future.fail(ar.cause());
+            }
+        });
+    }
+}
